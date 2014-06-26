4ba74f36899e6df18b36184f2d2c884b87356d79
textfield <textfield@12bb8179-5c4c-0410-a6dd-efa8bc14885c>
2014-06-26 17:55:13 +0000

    Work on PostgreSQL script.

---
+++ postgresql_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Alexis Megas 2014.
+
+pgdumpallfile="`which pg_dumpall 2> /dev/null`"
+
+if [ ! -r "$pgdumpallfile" -o ! -x "$pgdumpallfile" ]
+then
+    echo "Unable to locate pg_dumpall."
+    exit 1
+fi
+
+$pgdumpallfile --clean --file pg_dumpall.$$ "$@" 2>/dev/null
+
+if [ ! $? -eq 0 ]
+then
+    echo "pg_dumpall failed."
+    exit 1
+fi
+
+exit 0
