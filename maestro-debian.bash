bd48f6e0ac32066664aca193b2ebe6edfa204409
textbrowser@gmail.com <textbrowser@gmail.com>
2019-04-28 12:58:55 -0400

    Completed.

--- maestro-debian.bash
+++ maestro-debian.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
 # Alexis Megas, 2019.
-# Upgrade a set of machines.
+# Upgrade a set of Debian machines.
+
+# Please configure sudo on the host machines as follows:
+# user ALL=(root) NOPASSWD: /usr/bin/aptitude update, /usr/bin/aptitude upgrade
+
+# The software package aptitude is required!
+
+# List of machines along with appropriate accounts.
+
+declare -a hosts=("user@192.168.178.10"
+		  "user@192.168.178.12")
+
+for i in "${hosts[@]}"
+do
+    /usr/bin/ssh $i "sudo aptitude update && sudo aptitude upgrade"
+done
