9bdee77c38519a5ed6ec51c651b4abdc542b7f44
textbrowser <textbrowser@gmail.com>
2021-08-03 11:51:53 -0400

    New file.

---
+++ user_home_directories.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+# Alexis Megas.
+# Discover accounts and their respective home directories.
+
+for u in $(getent passwd | sort | awk 'BEGIN {FS=":"} ; {print $1}')
+do
+    e=$(getent passwd $u | rev | cut -d':' -f1 | rev)
+    h=$(getent passwd $u | cut -d':' -f6)
+
+    if [ "$e" == "/bin/false" ] ||
+       [ "$e" == "/sbin/nologin" ] ||
+       [ "$e" == "/usr/sbin/nologin" ] ||
+       [ -z "$e" ]
+    then
+	echo $u:/nonexistent
+    else
+	echo $u:$h
+    fi
+done
+
+exit 0
+
+# Using a temporary file.
+
+tmp=/tmp/accounts.$(date +'%Y%m%d%H%S').txt
+getent passwd | sort | awk 'BEGIN {FS=":"} ; {print $1}' > $tmp
+
+while read -r u
+do
+    e=$(getent passwd $u | rev | cut -d':' -f1 | rev)
+    h=$(getent passwd $u | cut -d':' -f6)
+
+    if [ "$e" == "/bin/false" ] ||
+       [ "$e" == "/sbin/nologin" ] ||
+       [ "$e" == "/usr/sbin/nologin" ] ||
+       [ -z "$e" ]
+    then
+	echo $u:/nonexistent
+    else
+	echo $u:$h
+    fi
+done < $tmp
+
+rm -f $tmp
