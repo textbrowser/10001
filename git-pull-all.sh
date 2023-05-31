5021c573c112e083a124e25e9e8e763aca68434b
textbrowser <textbrowser@gmail.com>
2023-05-31 11:50:45 -0400

    Local launch.

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 # Alexis Megas.
 
-# Errors are not displayed!
+# Discover all git repositories from the current directory and
+# issue git-pull for each discovered repository.
+# Errors are discarded!
 
-for directory in $(find ~ -name '.git' -type d -exec dirname {} \;)
+for directory in $(find . -name '.git' -type d -exec dirname {} \;)
 do
     echo "$directory:"
     git -C $directory pull $* 2>/dev/null
