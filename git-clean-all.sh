93e5ca0b9f74c6f1975a261e6c3e75acc2dc2e19
textbrowser <textbrowser@gmail.com>
2023-06-09 16:16:43 -0400

    New file.

---
+++ git-clean-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# Alexis Megas.
+
+# Discover all git repositories from the current directory and
+# issue git-clean for each discovered repository.
+# Errors are discarded!
+
+for directory in $(find . -name '.git' -type d -exec dirname {} \;)
+do
+    echo "$directory:"
+    git -C $directory clean -df $* 2>/dev/null
+done
+
+exit 0
