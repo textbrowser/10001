34c04c42ebb0ba38086bc4dc28e8fb35f1ff6f7f
textbrowser <textbrowser@gmail.com>
2025-11-02 07:29:44 -0500

    Safer sudo.

--- maestro-debian.bash
+++ maestro-debian.bash
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
 # Alexis Megas, 2019.
+# Alexis Megas, 2025. Safer sudo.
 
 # Update the packages on a set of Debian machines.
 
-# Please configure sudo on the host machines as follows:
-# user ALL=(root) NOPASSWD:
-# /usr/bin/aptitude update, /usr/bin/aptitude upgrade -y
-# The software package aptitude is required!
-
 # List of machines along with appropriate accounts.
 
-command1="sudo aptitude update 2>/dev/null"
-command2="sudo aptitude upgrade -y 2>/dev/null"
+command1="sudo apt update"
+command2="sudo apt upgrade -y"
 
-declare -a hosts=("192.168.178.10"
-		  "192.168.178.15")
+declare -a hosts=("192.168.178.45"
+		  "192.168.178.65")
 
 for i in "${hosts[@]}"
 do
-    /usr/bin/ssh $i "$command1 && $command2" 2>/dev/null
+    /usr/bin/ssh -t $i "$command1 && $command2" 2>/dev/null
 done
